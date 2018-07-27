0fac4d5232106f78477e6998abe8b01c0d209347
Vitaliy Timchenko <vitaliy.timchenko@privatbank.ua>
2018-07-27 15:03:46 +0300

    tourist preloader

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,6 @@
 import '../style/index.scss';
-import printMe from './print.js';
 
 console.log('*** *** *** *** ***');
 console.log('Site run in %s mode!',
   process.env.NODE_ENV ? process.env.NODE_ENV.toUpperCase() : 'UNKNOWN');
 console.log('*** *** *** *** ***');
-
-function component() {
-  let element = document.createElement('p');
-  element.innerHTML = 'Dynamically created element';
-  element.classList.add('marked');
-  element.onclick = printMe;
-
-  return element;
-}
-
-document.body.appendChild(component());
